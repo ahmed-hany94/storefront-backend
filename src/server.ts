6c5b07de162d2eebe1551ac4e2185bf97f0e76fc
ahmed-hany94 <singularity717@gmail.com>
2023-01-20 05:41:07 +0200

    setup

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,30 @@
-import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import { Pool } from "pg";
 
-const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+import dotenv from "dotenv";
 
-app.use(bodyParser.json())
+dotenv.config();
 
-app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
-})
+const { POSTGRES_HOST, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB } =
+  process.env;
+
+const client = new Pool({
+  host: POSTGRES_HOST,
+  database: POSTGRES_DB,
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+});
+
+const app: express.Application = express();
+const address: string = "0.0.0.0:3000";
+
+app.use(bodyParser.json());
+
+app.get("/", function (req: Request, res: Response) {
+  res.send("Hello World!");
+});
 
 app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
-})
+  console.log(`starting app on: ${address}`);
+});
